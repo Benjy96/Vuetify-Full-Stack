b8a587914e9050bbde65f86a6bde66e798340a23
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2019-12-31 11:33:54 +0000

    Add basic date validation to schedule time picker

--- client/src/components/TimeRangePicker.vue
+++ client/src/components/TimeRangePicker.vue
@@ -2,18 +2,47 @@
     <v-container class="grey lighten-2">
         <!-- v-bind:items because it tells Vue it's not a string -->
         <v-row class="green lighten-4">
-            <v-col class="brown lighten-1">
-                <v-select v-bind:items='["Monday", "Tuesday"]' label="Day"/>
-            </v-col>
-
-            <v-col class="brown lighten-2">
+            <v-col>
+            <v-form>
+                <v-select v-bind:items='["Monday", "Tuesday"]' label="Day" v-model="day"/>
                 <!-- v-text-field extends v-input: how do mixins work? -->
-                <v-text-field label="From" placeholder="09:00"/>
-            </v-col>
-
-            <v-col class="brown lighten-3">
-                <v-text-field label="To" placeholder="17:00"/>
+                <v-text-field label="From" placeholder="09:00" v-model="from" :rules="dateRules"/>
+                <v-text-field label="To" placeholder="17:00" v-model="to" :rules="dateRules"/>
+                <v-btn @click="validate">Save</v-btn>
+            </v-form>
             </v-col>
         </v-row>
     </v-container>
 </template>
+
+<script>
+import date from 'date-and-time';
+
+export default {
+    data() {
+        return {
+            day: 'Monday',
+            from: '09:00',
+            to: '17:00',
+            dateRules: [
+                v => !!v || 'A time is required!',
+                v => (v && v.length == 5 ) || 'Time must be in the "24:00" format'
+            ]
+        }
+    },
+    methods: {
+        validate() {
+            //TODO: Maybe a dropdown is better.....? (Although less control) 
+                //Any other date pickers? Get working for now in DB side though
+            let parsedFrom = date.parse(this.from, 'hh:mm');
+            let parsedTo = date.parse(this.to, 'hh:mm');
+
+            if(parsedFrom > parsedTo){
+                alert('From should be earlier than To!');
+            } else {
+                //Add to db...
+            }
+        }
+    }
+}
+</script>
