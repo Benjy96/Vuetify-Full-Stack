3f857c6d89a79e09d39007c8107ce8404b84dd8b
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2020-01-04 14:31:04 +0000

    Add rough plan to CRUD operations for calendar/dashboard

--- client/src/services/CalendarService.js
+++ client/src/services/CalendarService.js
@@ -5,12 +5,19 @@ class CalendarService {
 
     /** Cal-Day-CREATE */
     static async createBooking() {
-
+        //A booking will require 4 or 5 operations becuase of meta-data and admin availability.
+        
+        //Write to availability collection
+        //Write to more detailed owner bookings collection
+        //Read meta-data object
+        //Read current day's remaining hours object
+            //Sum remaining hours + admin hours if exists
+                //If no remaining hours, add day to unavailable days meta-data
     }
 
     /** Cal-Day-READ */
     static async getBookings() {
-
+        //Read from availability collection
     }
 
     /** Cal-Day-DELETE */
@@ -24,6 +31,6 @@ class CalendarService {
 
     /** Cal-Month-READ */
     static async getUnavailableDays() {
-
+        //Read from meta-data doc.
     }
 }
--- client/src/services/OwnerService.js
+++ client/src/services/OwnerService.js
@@ -4,14 +4,22 @@ class OwnerService {
         Owner/Business PoV CRUD Operations.
     */
 
+    /** CREATE */
     static async createAdminBooking() {
-
+        //Add to meta-data document.
     }
 
+    /** READ */
     static async getUpcomingBookings() {
 
     }
 
+    /** READ */
+    static async getAdminBookings() {
+        //Fetch from meta-data document.
+    }
+
+    /** DELETE/UPDATE */
     static cancelBooking() {
 
     }
