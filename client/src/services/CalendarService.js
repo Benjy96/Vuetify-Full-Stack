2dfff2d92cd6be59a3dc8d32fb5d232197e929ce
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2020-01-04 14:16:48 +0000

    Add method signatures for customer/owner PoV CRUD operations.

---
+++ client/src/services/CalendarService.js
@@ -0,0 +1,29 @@
+class CalendarService {
+    /*
+        Day PoV CRUD Operations.
+    */
+
+    /** Cal-Day-CREATE */
+    static async createBooking() {
+
+    }
+
+    /** Cal-Day-READ */
+    static async getBookings() {
+
+    }
+
+    /** Cal-Day-DELETE */
+    static async cancelBooking() {
+
+    }
+
+    /* 
+        Month PoV CRUD Operations.
+    */
+
+    /** Cal-Month-READ */
+    static async getUnavailableDays() {
+
+    }
+}
---
+++ client/src/services/OwnerService.js
@@ -0,0 +1,18 @@
+class OwnerService {
+
+    /*
+        Owner/Business PoV CRUD Operations.
+    */
+
+    static async createAdminBooking() {
+
+    }
+
+    static async getUpcomingBookings() {
+
+    }
+
+    static cancelBooking() {
+
+    }
+}
