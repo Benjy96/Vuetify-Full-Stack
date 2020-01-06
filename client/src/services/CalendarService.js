e02f6b6b618f95f55f677ca0a52062d7f9d9e79f
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2020-01-06 10:04:53 +0000

    Code formatting

--- client/src/services/CalendarService.js
+++ client/src/services/CalendarService.js
@@ -95,18 +95,6 @@ class CalendarService {
         }
     }
 
-    static async getAdminBookings(uid, year, month) {
-        let metaDataDocRef = await db.collection(`/businesses/${uid}/availability/${year}/month/`).doc(`${month}`).get();
-        let admin_bookings = [];
-        if(metaDataDocRef.exists) {
-            admin_bookings = metaDataDocRef.data().admin_bookings;
-            return admin_bookings;
-        } else {
-            //TODO: Check traversy apps - waht does he do when null? exception? message?
-            return null;
-        }
-    }
-
     /** Cal-Day-DELETE */
     static async cancelBooking() {
 
@@ -133,6 +121,21 @@ class CalendarService {
         }
 
         return dateObj;
+    }
+
+    /*
+        Month / Day PoV CRUD Operations
+    */
+    static async getAdminBookings(uid, year, month) {
+        let metaDataDocRef = await db.collection(`/businesses/${uid}/availability/${year}/month/`).doc(`${month}`).get();
+        let admin_bookings = [];
+        if(metaDataDocRef.exists) {
+            admin_bookings = metaDataDocRef.data().admin_bookings;
+            return admin_bookings;
+        } else {
+            //TODO: Check traversy apps - waht does he do when null? exception? message?
+            return null;
+        }
     }
 }
 
