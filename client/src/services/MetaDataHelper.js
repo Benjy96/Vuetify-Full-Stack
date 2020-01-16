0b6e73287768f3ab0a2df68719b342cfa097dfe4
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2020-01-16 18:42:15 +0000

    Add pseudocode for determining remaining time
    Upon the final test I realised the complexity was more than I had anticipated:
    
    - [x] Admin booking 2020-01-01 17:00 -> 2020-01-01 17:00 = TRUE
    - [x] Admin booking  2020-01-01 00:00 -> 2020-01-03 = FALSE
    - [x] Admin booking 2020-01-01 00:00 -> 2020-01-02 = FALSE
    - [x] Admin booking 2020-01-01 00:00 -> 2020-01-01 00:05 = TRUE
    - [x] Admin booking 2020-01-01 00:00 -> 2020-01-01 14:00 = TRUE
    - [x] Admin booking 2019-10-01 17:00 -> 2020-01-01 24:00 = FALSE
    - [x] Admin booking 2019-10-01 17:30 -> 2020-01-01 17:30, Wednesday Regular Hours 09:00->17:00 = FALSE
    - [x] Admin booking 2019-10-01 17:30 -> 2020-01-01 17:00, Wednesday Regular Hours 09:00->17:00 = FALSE
    - [ ] Admin booking 2019-10-01 17:30 -> 2020-01-01 16:30, Wednesday Regular Hours 09:00->17:00 = TRUE
    
    In the last commit, the code is subtracting admin bookings and regular hours from 24, but this means a time, such as 15:00, is being subtracted "twice". That is, if an admin booking covers 15:00, and a regular hour covers that, the algorithm is removing 15:00 for each type of time.
    
    I am going to handle this by creating an array of "Regular Hours", each of which will have a number representing the time it has left. We will then go through bookings and admin bookings, removing time from that range if the booking is in/affects that range of time

--- client/src/services/MetaDataHelper.js
+++ client/src/services/MetaDataHelper.js
@@ -4,16 +4,59 @@ import { daysOfWeek } from '../DateUtils';
 
 class MetaDataHelper {
 
-    //TODO - what are the from/to? Rounded up and down from/to day if a range?
-    // static async markDaysUnavailable(uid, from, to) {
-    //     //Example meta-data doc path: /businesses/6c6qWcNvsOhBpF0CgUox4LsG2v62/availability/2020/month/01
+    static isTimeLeft(ranges) {
+        let y = 0;
+        for(var x in remainingRanges) {
+            if(remainingRanges[x] == 0){
+                y++;
+                if(y == remainingRanges.length) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
-    //     //1. Get every month involved in the from->to
-    //     //2. For each month, access firestore meta-data document
-    //         //3. Add day to unavailable list
-            
-    // }
+    static async getAvailableTime(uid, date) {
+        let remainingRanges = 0;
 
+        let regularHoursDoc = await db.collection(`/businesses/${uid}/availability`).doc(`regular`).get();
+        let regularHours = [];
+
+        let weekday = daysOfWeek[new Date(date).getDay()];
+        if(regularHoursDoc.exists) {
+            regularHours = regularHoursDoc.data()[weekday];
+            alert(JSON.stringify(regularHours));
+            for(var range in regularHours) {
+                //Remove the hours OUTSIDE the regular hours range
+                remainingRanges += DateUtils.calcFromToDifference(regularHours[range].from, regularHours[range].to);
+            }
+        }
+        return remainingRanges;
+    }
+
+    /**
+     * @returns the amount of available time a user has on a date
+     */
+    static async getRegularHoursWithTimeRemaining(uid, date) {
+        let remainingRanges = [];
+
+        let regularHoursDoc = await db.collection(`/businesses/${uid}/availability`).doc(`regular`).get();
+        let regularHours = [];
+
+        let weekday = daysOfWeek[new Date(date).getDay()];
+        if(regularHoursDoc.exists) {
+            regularHours = regularHoursDoc.data()[weekday];
+            for(var range in regularHours) {
+                let timeRange = regularHours[range].from + regularHours[range].to;
+                let rangeAndTime = {
+                    [timeRange]: DateUtils.calcFromToDifference(regularHours[range].from, regularHours[range].to)
+                }
+                remainingRanges.push(rangeAndTime);
+            }
+        }
+        return remainingRanges;
+    }
     /*
         TODO: What if we instead have a day collection, where you add different "types" to it,
         like an admin booking & customer booking?
@@ -25,78 +68,144 @@ class MetaDataHelper {
     * @returns {boolean}
     */
     static async isDateAvailable(uid, date) {
-        //Prepare to count remaining hours of a day
-        let remainingHours = 24;
+        //1 - Set remaining hours based on regular hours
+        let remainingRanges = await this.getRegularHoursWithTimeRemaining(uid, date);
 
-        let year = DateUtils.getYearFromDate(date);
-        let month = DateUtils.getMonthFromDate(date);
-        let day = DateUtils.getDayFromDate(date);
+        if(remainingRanges.length > 0) {
+            let year = DateUtils.getYearFromDate(date);
+            let month = DateUtils.getMonthFromDate(date);
+            let day = DateUtils.getDayFromDate(date);
 
-        //1. Check bookings for day
-        let dayDoc = await db.collection(`/businesses/${uid}/availability/${year}/month/${month}/days`).doc(`${day}`).get();
-        let customer_bookings = [];
-        if(dayDoc.exists) {
-            customer_bookings = dayDoc.data().customer_bookings;
+            //1. Check bookings for day
+            let dayDoc = await db.collection(`/businesses/${uid}/availability/${year}/month/${month}/days`).doc(`${day}`).get();
+            let customer_bookings = [];
+            if(dayDoc.exists) {
+                customer_bookings = dayDoc.data().customer_bookings;
 
-            for(var bookedRanges in customer_bookings){
-                remainingHours -= DateUtils.calcFromToDifference(customer_bookings[bookedRanges].from, 
-                    customer_bookings[bookedRanges].to);
-            }
+                for(let range in remainingRanges) {
+                    for(var bookedRanges in customer_bookings) {
+                        let bookingFromTime = customer_bookings[bookedRanges].from;
+                        let bookingToTime = customer_bookings[bookedRanges].to;
 
-            if(remainingHours <= 0) {
-                return false;
-            }
-        }
-        
-        //2. Check admin bookings for day
-        let adminDoc = await db.collection(`/businesses/${uid}/bookings`).doc('admin').get();
-        let admin_bookings = [];
-        if(adminDoc.exists) {
-            admin_bookings = adminDoc.data().admin_bookings;
+                        if(DateUtils.timeWithinHourRange(bookingFromTime, range) && 
+                            DateUtils.timeWithinHourRange(bookingToTime, range)) 
+                        {
+                            remainingRanges[range] -= DateUtils.calcFromToDifference(bookingFromTime, bookingToTime);
+                        }
+                    }
+                }
 
-            //Get the relevant admin booking for this day
-            for(var i in admin_bookings) {
-                //The day is in the middle of a large range of time
-                if(DateUtils.dateBetween(date, admin_bookings[i].fromDate, admin_bookings[i].toDate)) {
+                if(this.isTimeLeft(remainingRanges) == false) {
                     return false;
                 }
-
-                //Admin Booking on one day
-                if(admin_bookings[i].fromDate == date && admin_bookings[i].toDate == date) {
-                    remainingHours -= DateUtils.calcFromToDifference(admin_bookings[i].fromTime, admin_bookings[i].toTime);
-                } else if(admin_bookings[i].fromDate == date) {
-                    //The day is on the admin booking "from" date
-                    remainingHours -= DateUtils.calcFromToDifference(admin_bookings[i].fromTime, "24:00");
-                } else if(admin_bookings[i].toDate == date) {
-                    //The day is on admin booking "to" date
-                    remainingHours -= DateUtils.calcFromToDifference("00:00", admin_bookings[i].toTime);
-                }
-
-                if(remainingHours <= 0) {
-                    return false;
-                }
-            }   
-        }
-
-        //3. Check regular hours for day
-        let regularHoursDoc = await db.collection(`/businesses/${uid}/availability`).doc(`regular`).get();
-        let regularHours = [];
-
-        let weekday = daysOfWeek[new Date(date).getDay()];
-        if(regularHoursDoc.exists) {
-            regularHours = regularHoursDoc.data()[weekday];
+            }
             
-            for(var range in regularHours){
-                remainingHours -= DateUtils.calcFromToDifference(regularHours[range].from, regularHours[range].to);
+            //2. Check admin bookings for day
+            let adminDoc = await db.collection(`/businesses/${uid}/bookings`).doc('admin').get();
+            let admin_bookings = [];
+            if(adminDoc.exists) {
+                admin_bookings = adminDoc.data().admin_bookings;
+
+                //Get the relevant admin booking for this day
+                for(var i in admin_bookings) {
+                    //The day is in the middle of a large range of time
+                    if(DateUtils.dateBetween(date, admin_bookings[i].fromDate, admin_bookings[i].toDate)) {
+                        return false;
+                    }
+
+                    //Admin Booking on one day
+                    if(admin_bookings[i].fromDate == date && admin_bookings[i].toDate == date) {
+                        //remainingRanges -= DateUtils.calcFromToDifference(admin_bookings[i].fromTime, admin_bookings[i].toTime);
+
+                        /* TODO: Check for admin bookings
+
+                            An admin booking can be outside or inside the range
+
+                            We need to subtract the time within both ranges
+
+                            e.g.:
+
+                            Start with remaining: 8.5
+
+                            regular 09:00->17:30
+                            admin 00:00->14:00
+                            
+                            Need to subtract 5 hours. Remaining 3.5
+
+                        */
+
+                        
+                        for(let range in remainingRanges) {
+
+                        /*
+
+                        For each regular hour range, (admin / reg):
+
+                        This is how we can do it: Use logic to determine the inside and outisde of each range:
+                        
+                            10:00->14:00 / 09:00->12:30 = 
+                                if admin to > reg to 
+                                &&
+                                if admin from > reg from:
+                                    count from admin from -> reg to
+                                && admin from > reg from:
+                                    count from admin from to reg to
+                                    subtract from this range's remaining hours
+
+
+                                    ** We only need to count the hours in the reg range! **
+                                    I.e., 
+                                    count to reg To if largest To
+                                    count from reg from if admin from >= reg from
+
+                                    Simply need to determine the largest/smallest from and to:
+
+                                    if adFrom > regFrom, countFrom = regFrom (reg hours are what we're subtracting from)
+                                    else countFrom = adminFrom
+                                    if adTo > regTo, countTo = regTo
+                                    else countTo = adTo
+
+                        */
+
+                        }
+
+
+
+
+                    } else if(admin_bookings[i].fromDate == date) {
+                        //The day is on the admin booking "from" date
+                        //remainingRanges -= DateUtils.calcFromToDifference(admin_bookings[i].fromTime, "24:00");
+
+
+
+
+
+
+
+                    } else if(admin_bookings[i].toDate == date) {
+
+                        //remainingRanges -= DateUtils.calcFromToDifference("00:00", admin_bookings[i].toTime);
+
+
+
+
+
+                    }
+
+                    if(this.isTimeLeft(remainingRanges) == false) {
+                        return false;
+                    }
+                }   
+            }
+
+            //4. Time left?
+            if(this.isTimeLeft(remainingRanges) == false) {
+                return false;
+            } else {
+                return true;
             }
         }
-
-        //4. Time left?
-        if(remainingHours <= 0) {
-            return false;
-        } else {
-            return true;
-        }
+        return false;
     }
 }
 
