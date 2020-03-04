78d33ef08e9f80ef58398b6f9d08bfa1babd0048
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2020-03-04 15:46:13 +0000

    Move theme to vuetify plugin

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import theme from './theme';
 
 Vue.use(Vuetify);
+
+const theme = {
+    primary: '#4CAF50',
+    secondary: '#9C27b0',
+    accent: '#9C27b0',
+    info: '#00CAE3',
+}
 
 export default new Vuetify({
     theme: {
