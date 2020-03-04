8b7140721e71b32c939fa9f053882cf40953a4cf
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2020-03-04 10:00:33 +0000

    Add & Register Global Base Components

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
 import router from './router'
+
+// Initialise Firebase
+import './firebaseInit';
+
+// Plugins
+import vuetify from './plugins/vuetify';
 import languageSwitcher from './plugins/languageSwitcher';
 
-import './firebaseInit';
+// Globally register our generic components
+import './globalComponents';
 
 Vue.config.productionTip = false
 
