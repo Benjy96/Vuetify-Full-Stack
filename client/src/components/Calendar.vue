01324727ac1cb998c3c948085f4caff231beb0d9
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2020-03-05 08:19:40 +0000

    Bug Fix: Infinite Load if no unavailableDays

--- client/src/components/Calendar.vue
+++ client/src/components/Calendar.vue
@@ -523,16 +523,14 @@ export default {
 
       let data = await CustomerService.getMonthAvailabilityData(this.id, year, month);
 
-      this.unavailableDays[year][month] = data.unavailableDays;
-      if(data.irregularAvailability) {
-        if(this.events[year][month] == null) {
-          this.events[year][month] = [];
+      if(data && data.unavailableDays) {
+        this.unavailableDays[year][month] = data.unavailableDays;
+        if(data.irregularAvailability) {
+          if(this.events[year][month] == null) {
+            this.events[year][month] = [];
+          }
+          this.events[year][month] = data.irregularAvailability;
         }
-        this.events[year][month] = data.irregularAvailability;
-        // this.events[year][month] = [];
-        // data.irregularAvailability.forEach(x => { 
-        //   this.events.push(x);
-        // });
       }
     },
     async getDayBookings(date) {
